ece48273af1f5bdb57400f58db500d6597f9c5d5
VSGayatri <gayatri.vsb@outlook.com>
2023-07-04 13:17:13 +0530

    Comments added

--- Basic Shell Scripts/01-script1.sh
+++ Basic Shell Scripts/01-script1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 clear
+# This is the commented line for the below line
+# I defined about the colour and the value of the colour.
 user="Mahatma Gandhi"
 echo "He is one of the fighter to get freedom, $user"
 echo
