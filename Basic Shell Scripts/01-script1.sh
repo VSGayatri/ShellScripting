46b37fd5d161adf97fbd2742235fac2f9d6d871c
VSGayatri <gayatri.vsb@outlook.com>
2023-07-04 12:18:32 +0530

    working on first shell script

---
+++ Basic Shell Scripts/01-script1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+user="Mahatma Gandhi"
+echo "He is one of the fighter to get freedom, $user"
+echo
+w
+echo
+echo "I am Giving one colour, $colour"
+colour="Blue"
+echo "The value of the colour, $value"
+value=10
+echo 
