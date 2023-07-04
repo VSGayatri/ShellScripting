a551c9160716795423b441726bcbf411adf075ad
VSGayatri <gayatri.vsb@outlook.com>
2023-07-04 13:48:42 +0530

    To check the Debug Mode

---
+++ Basic Shell Scripts/script2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo "Git was originally authored by Linus Torvalds in 2005"
+echo  "For development of the Linux kernel, with other kernel developers".
+echo 
+set -x   #Activate debugging from here
+w
+set +x  # stop debugging from here
+echo "Here i am considering two variables"
+Puppy="White"
+value="01"
+echo "Display the string Name, $Puppy"
+echo "Display the Value of the string, $value"
+echo
